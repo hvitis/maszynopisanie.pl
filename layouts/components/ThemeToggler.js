130b1dcbe6e75cb577c9c05a6c24fa1ecb33fe10
Adam Piskorek <adam.piskorek.pl@gmail.com>
2022-10-07 18:30:21 +0200

    Show button only on dev

--- layouts/components/ThemeToggler.js
+++ layouts/components/ThemeToggler.js
@@ -7,19 +7,24 @@ const ThemeToggler = () => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
-  return (
-    <button
-      className="flex h-8 w-8 items-center justify-center rounded-lg bg-blue-100 ring-blue-400 transition-all duration-300 hover:ring-2 focus:outline-none dark:bg-slate-200"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      aria-label="Toggle Dark Mode"
-    >
-      {theme === 'light' ? (
-        <MoonIcon className="h-5 w-5 text-white-500" />
-      ) : (
-        <SunIcon className="h-5 w-5 text-yellow-700" />
-      )}
-    </button>
-  );
+
+  if (process.env.NODE_ENV === 'development') {
+    return (
+      <button
+        className="flex h-8 w-8 items-center justify-center rounded-lg bg-blue-100 ring-blue-400 transition-all duration-300 hover:ring-2 focus:outline-none dark:bg-slate-200"
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        aria-label="Toggle Dark Mode"
+      >
+        {theme === 'light' ? (
+          <MoonIcon className="text-white-500 h-5 w-5" />
+        ) : (
+          <SunIcon className="h-5 w-5 text-yellow-700" />
+        )}
+      </button>
+    );
+  } else {
+    null;
+  }
 };
 
 export default ThemeToggler;
